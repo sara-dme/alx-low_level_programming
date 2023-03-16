4b63e3f62a797f6d9c0f24ca10b531e14c713723
sara-dme <s.daoumy@gmail.com>
2023-03-16 12:00:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * * main - write to stdout
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
