9cf7747b3ca360d86828b704bdf8bd1c8d5e0f95
sara-dme <s.daoumy@gmail.com>
2023-03-16 12:07:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * * main - write to stdout
+ * * main - write
  * Return: Always 0
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
 	return (1);
 }
