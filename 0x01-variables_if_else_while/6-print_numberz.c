b259968ea8b7d3bc7add9bd59cdeeae803b67742
sara-dme <s.daoumy@gmail.com>
2023-03-17 09:08:21 -0700

    numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main 
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
