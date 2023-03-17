491decc402c132fcb3f46e5ef790e53c125d4d99
sara-dme <s.daoumy@gmail.com>
2023-03-17 11:21:45 -0700

    ./100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 1 ; b >= 9 ; b++)
+		{
+			if ((a != b) && (a < b))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
