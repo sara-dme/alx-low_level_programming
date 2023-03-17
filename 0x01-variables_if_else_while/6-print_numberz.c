813564d93a0d3cb74eae87028721102152081a07
sara-dme <s.daoumy@gmail.com>
2023-03-17 09:34:30 -0700

    numnerz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main 
+ * main Print all single digit numbers using only putchar
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		putchar(i);
+		putchar(i + '0' );
 	}
 	putchar('\n');
 	return (0);
