ccf2f7d46934c0801675c8e2fd994449858d2d6e
sara-dme <s.daoumy@gmail.com>
2023-03-17 12:06:25 -0700

    ./101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,27 @@
  */
 int main(void)
 {
-        int a, b,c;
+	int a, b, c;
 
-        for (a = 0 ; a <= 9 ; a++)
-        {
-                for (b = 1 ; b <= 9 ; b++)
-                {
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 1 ; b <= 9 ; b++)
+		{
 			for (c = 2 ; c <= 9 ; c++)
 			{
-                        if ((a != b) && (a < b) && (b < c ))
-                        {
-                                putchar(a + '0');
-                                putchar(b + '0');
+				if ((a != b) && (a < b) && (b < c ))
+				{
+				putchar(a + '0');
+				putchar(b + '0');
 				putchar(c + '0');
-                                if ((a != 7) || (b != 8) || (c != 9))
-                                {
-                                putchar(',');
-                                putchar(' ');
-                                }
-			}
-                        }
-                }
-        }
-        putchar('\n');
+				if ((a != 7) || (b != 8) || (c != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				}
+                	}
+		}	
+		putchar('\n');
         return (0);
 }
