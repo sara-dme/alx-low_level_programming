07240bcc1c235ac217f30bbb9c606aba22216463
sara-dme <s.daoumy@gmail.com>
2023-03-17 11:36:22 -0700

    ./100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int a,b;
+	int a, b;
 
 	for (a = 0 ; a <= 9 ; a++)
 	{
 		for (b = 1 ; b <= 9 ; b++)
 		{
-			if ((a != b) || (a < b))
+			if ((a != b) && (a < b))
 			{
 				putchar(a + '0');
 				putchar(b + '0');
