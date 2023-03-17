3080099db71ae21f8559a7ae6c383f8b091e72ec
sara-dme <s.daoumy@gmail.com>
2023-03-17 10:53:18 -0700

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
- /**
- * main - Entry point
- * Description: 'the program's description'
- * @parameter: describe the parameter
+/**
+ *main - Entry point
+ *
  * Return: Always 0 (Success)
  */
+ 
 int main(void)
 {
 	int i;
