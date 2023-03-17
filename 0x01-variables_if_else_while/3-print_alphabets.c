eff83cb8769359051603dd2f2b2c9f3a9892a2cb
sara-dme <s.daoumy@gmail.com>
2023-03-17 08:06:33 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char ch;
+
+	while (c < 'z') {
+		putchar(c);
+		c++;
+       }
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+		
+	return (0);
+}
