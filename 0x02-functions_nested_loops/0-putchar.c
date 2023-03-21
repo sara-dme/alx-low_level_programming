a6486d2b6bde389df6f65614fc5a4b011a569849
sara-dme <s.daoumy@gmail.com>
2023-03-21 07:47:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Rsturn : Always 0.
+ */
+
+void main(int)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		putchar(*c);
+		c++
+	}
+	putchar ('\n');
+return (0);
+}
