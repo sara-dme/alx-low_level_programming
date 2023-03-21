88ab809a79715dd9817ce73bb854605074e8611b
sara-dme <s.daoumy@gmail.com>
2023-03-21 11:01:13 -0700

    alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
 
 void print_alphabet_x10(void)
 {
-	char c ='a';
+	char c = 'a';
 	int i;
 
-	for (i =0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		while (c <= 'z')
 		{
 		_putchar(c);
 		c++;
-		}		
-	}
+		}
 	_putchar('\n');
+	}
 }
