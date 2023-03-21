a4a80194911197d34cfe7bcfbdf855b00724c114
sara-dme <s.daoumy@gmail.com>
2023-03-21 10:45:52 -0700

    alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i =0 ; i < 10 ; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
