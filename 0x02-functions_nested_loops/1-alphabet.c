f1b5bedc964867cf55391db9c53f64453e6ab0f8
sara-dme <s.daoumy@gmail.com>
2023-03-21 08:42:37 -0700

    fixed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
