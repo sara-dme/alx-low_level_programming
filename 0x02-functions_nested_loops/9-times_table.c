8e660fd75938685424a2f8c8c7799fab7cd59d46
sara-dme <s.daoumy@gmail.com>
2023-03-22 01:43:23 -0700

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,15 +12,26 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			res = i * j;
-			if (res < 10) && (j != 9)
+			
+			if (j == 0)
 			{
-				_putchar(res + '0');
+				_putchar(0);
+			}
+			else if ((res < 10) && (j != 0))
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-			}
-			else if (j == 9)
 				_putchar(res + '0');
+			}
+			else if (res >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
