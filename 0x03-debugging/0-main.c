abbb1c016157c9dc78e2ff299e4a39254e1e4e4b
sara-dme <s.daoumy@gmail.com>
2023-03-22 09:03:41 -0700

    test main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is pos or neg
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 98;
+	positive_ornegative(i);
+
+	return (0);
+}
+
