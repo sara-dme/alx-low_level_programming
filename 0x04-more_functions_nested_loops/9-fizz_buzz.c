3ea96a1b61329bf7145c94e1304cd0fd57f15351
sara-dme <s.daoumy@gmail.com>
2023-03-23 07:07:07 -0700

    fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 3 != 0 && i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%d",i);
-		printf("\n");
+			printf("%d ",i);
 	}
+	printf("\n");
 	return (0);
 }
