f811a47e546c29cbae5896d690c062779c4842ef
sara-dme <s.daoumy@gmail.com>
2023-03-23 07:23:24 -0700

    fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,11 @@ void print_triangle(int size)
 		int i;
 		int j;
 
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = size; j <= i; j--)
+			for (j = i; j < size; j++)
+				 _putchar(' ');
+			for (j = 1; j <= i; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
