cafa2389e9bb7ff5ab7e7463d7d2f329f0f917df
sara-dme <s.daoumy@gmail.com>
2023-03-23 04:28:49 -0700

    more num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
