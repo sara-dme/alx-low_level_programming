320fad68494f330f240323c63d23c5ea8595f58a
sara-dme <s.daoumy@gmail.com>
2023-03-23 06:50:31 -0700

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,12 @@ void print_diagonal(int n)
 	{
 		int i;
 		int j;
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 				_putchar(' ');
-			_putchar('/');
-		}
+		_putchar('\\');
 		_putchar('\n');
+		}
 	}
 }
