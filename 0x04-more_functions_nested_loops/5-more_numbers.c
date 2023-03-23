865bef8e9fa61e33a5e06390c2c3cf33140cd23c
sara-dme <s.daoumy@gmail.com>
2023-03-23 06:21:15 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,15 @@ void more_numbers(void)
 	for (j = 0; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
-			_putchar(i + '0');
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+				_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
