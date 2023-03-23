18da9e631c3d8c55b22507f688501e9820b1037c
sara-dme <s.daoumy@gmail.com>
2023-03-23 04:04:11 -0700

    num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 /**
  * print_numbers -  a function that prints the numbers, from 0 to 9
  * followed by a new line.
- * Return : 0 
  */
 void print_numbers(void)
 {
@@ -10,7 +9,7 @@ void print_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
