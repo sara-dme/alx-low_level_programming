025440ff056c265e926dd8af141176ff1affed03
sara-dme <s.daoumy@gmail.com>
2023-03-27 15:54:22 -0700

    fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,18 +8,17 @@
 int _atoi(char *s)
 {
 	int i, sn = 1;
-	int res;
+	unsigned int res = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		
+		if (s[i] == '-')
+			  sn = sn * -1;
 		if ((s[i] >= '0') && (s[i] <= '9'))
 			{
-			if (s[i] == '-')
-				sn = sn * -1;
 		res = (res * 10) + (s[i] - '0');
 			}
 	}
 	res = res * sn;
-return (res);	
+return (res);
 }
