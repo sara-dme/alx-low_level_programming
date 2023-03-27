79abb946c7a0aa61ab2f42c79e415c8fcd807f60
sara-dme <s.daoumy@gmail.com>
2023-03-27 15:16:59 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,9 @@ void print_rev(char *s)
 	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{	
+	{
 	}
-	for (j = i - 1; s[j] >= 0; j--)
+	for (j = i - 1; s[j] > 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
