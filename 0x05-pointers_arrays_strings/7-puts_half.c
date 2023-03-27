ff9d4a672e9148fa8d6c3963117949453401cd42
sara-dme <s.daoumy@gmail.com>
2023-03-27 14:53:25 -0700

    fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,17 @@
 void puts_half(char *str)
 {
 	int i, len, n;
-
+	
+	n = 0;
 	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		len = len + 1;
-		n = (len - 1) / 2;
+	}
+	n = (len - 1) / 2;
 	for (i = n; i < len ; i++)
+	{
 		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
