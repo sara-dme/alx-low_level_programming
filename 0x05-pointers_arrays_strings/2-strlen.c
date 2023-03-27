ee8c6831ed1ac45e05940af19b1595c8c7379545
sara-dme <s.daoumy@gmail.com>
2023-03-27 05:12:50 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,18 @@
 
 /**
  * _strlen - returns the length of a string.
- * @*s: char variable
+ * @s: char variable
  * Return: length of a string
  */
 int _strlen(char *s)
 {
 	int i;
 	int len;
+	char str[];
 
-	i = 0;
-	while (*s)
-	len = i + 1;
+	i = 0 ;
+	str[] = *s;
+	for (i = 0; str[i] != 0; i++)
+		len = i + 1;
 	return (len);
 }
