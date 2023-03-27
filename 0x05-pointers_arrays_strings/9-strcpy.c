8adfab0826a7f84df7534423599b629f7d965dc5
sara-dme <s.daoumy@gmail.com>
2023-03-27 15:22:20 -0700

    fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,5 +9,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	
+	int i, len;
+
+	for (len = 0; src[len] != '\0'; len++)
+	{
+	}
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+return (dest);	
 }	
