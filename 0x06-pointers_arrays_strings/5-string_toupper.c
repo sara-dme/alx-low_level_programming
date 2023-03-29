aa55ef63b0518bb91d6a79820e8a8bfe176f90be
sara-dme <s.daoumy@gmail.com>
2023-03-29 15:15:38 -0700

    fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,12 +3,12 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
  * @a: char pointer
- * Return: char 
+ * Return: char
  */
 char *string_toupper(char *a)
 {
 	int i;
-	
+
 	i = 0;
 	while (a[i] != '\0')
 	{
