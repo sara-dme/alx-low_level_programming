46f33fc349bd27b7be74fe5cc3cecac66b884847
sara-dme <s.daoumy@gmail.com>
2023-03-29 14:26:33 -0700

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, len, len2, j;
-	
-	j = 0;
-	for (len = 0; s1[len] != '\0'; len++)
+	int res = 0;
+
+	while (*s1 == *s2 && *s1 != '\0')
 	{
+		s1++;
+		s2++;
 	}
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{
-	}
-	for (i = 0; i < len; i++)
-	{
-		if (s1[i] == s2[i])
-			j = j + 0;
-		else if (s1[i] > s2[i])
-			j = j + 1;
-		else
-			j = -1 * (j + 1);
-	}
-	return (j);
+	if (s1 != s2)
+		res = *s1 - *s2;
+
+	return (res);
 }
