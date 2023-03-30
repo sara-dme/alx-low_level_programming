2698a4d92507b5a35c7abd0e6d946c8097147ec9
sara-dme <s.daoumy@gmail.com>
2023-03-30 10:59:19 -0700

    fixed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,9 +13,9 @@ char *rot13(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; a[j] != '\0'; i++)
+		for (j = 0; aa[j] != '\0'; j++)
 		{
-			if a[i] == aa[j]
+			if (a[i] == aa[j])
 			{
 				a[i] = b[j];
 				break;
