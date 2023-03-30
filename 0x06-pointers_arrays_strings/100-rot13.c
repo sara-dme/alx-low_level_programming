9c87a60b9a16be6fc4c0834ff747f1e13a956dc5
sara-dme <s.daoumy@gmail.com>
2023-03-30 04:25:56 -0700

    fixed

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @a: char pointer
+ * Return: char pointer
+ */
+char *rot13(char *a)
+{
+	while(*a)
+	{
+		while ((*a >= 'a' && *a <= 'z') || (*a >= 'A' && *a <= 'Z'))
+		{
+		if ((*a >= 'a' || *a >= 'A') && (*a < 'n' || *a < 'N'))
+		{
+			*a = *a + 13;
+			break;
+		}
+		*a = *a - 13;
+		break;
+		}
+		a++;
+	}
+	return (a);
+}
