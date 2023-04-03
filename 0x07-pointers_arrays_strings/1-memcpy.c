4c945291749047593715c52c1294169166751531
sara-dme <s.daoumy@gmail.com>
2023-04-03 07:03:09 -0700

    fixed

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: merory area to copy from
+ * @src: memory area to copy in
+ * @n: byte to copies
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	
+}
