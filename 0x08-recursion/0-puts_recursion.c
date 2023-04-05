0372f3d0435542ce8cc51a940e17022cbf336304
sara-dme <s.daoumy@gmail.com>
2023-04-05 06:43:36 -0700

    second file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,6 @@
  */
 void _puts_recursion(char *s)
 {
-	
 	if (*s != '\0')
 	{
 		_putchar(*s);
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-
-}
-/**
- * revers - reverse a string 
- * @s: string to reverse
- */
-char *rev(char *s)
-{
-	
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
