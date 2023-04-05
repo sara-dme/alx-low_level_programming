652a9a2cc38cc3c664dc8bcad6f18cbe12637559
sara-dme <s.daoumy@gmail.com>
2023-04-05 07:04:24 -0700

    fixed

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
 /**
  * factorial - returns the factorial of a given number.
  * @n: given num
- * return: factoriel of n
+ * Return: factoriel of n
  */
 int factorial(int n)
 {
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - eturns the value of x raised to the power of y
+ * @x: int value
+ * @y: power int
+ * Return: int value
+ */
+int _pow_recursion(int x, int y)
+{
+
+}
