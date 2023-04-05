af3aaf71be2474b406615dcd9561a1ad2815bb42
sara-dme <s.daoumy@gmail.com>
2023-04-05 06:37:36 -0700

    fixed

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string to br printed
+ */
+void _print_rev_recursion(char *s)
+{
+
+}
+/**
+ * revers - reverse a string 
+ * @s: string to reverse
+ */
+char *rev(char *s)
+{
+	
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
