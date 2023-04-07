e55b5c6208ff68c78a2441bbc2e7fd67475f751f
sara-dme <s.daoumy@gmail.com>
2023-04-07 05:51:51 -0700

    fixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,16 @@
-#include "holberton.h"
-void _puts(char *s)
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: char variable
+ */
+void _puts(char *str)
 {
-	putchar('h');
+	int i;
+
+	i = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
