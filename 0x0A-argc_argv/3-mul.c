65c233fd149daf3257d13fae4d5d7a8c86f61ded
sara-dme <s.daoumy@gmail.com>
2023-04-08 05:36:42 -0700

    fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,12 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
 	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	return (0);
 }
