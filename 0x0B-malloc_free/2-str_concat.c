3db6145fe273d770d3e3423d610d347e2dbfd822
sara-dme <s.daoumy@gmail.com>
2023-04-11 05:23:09 -0700

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,14 +14,20 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 
 	if (s1 == 0 && s2 == 0)
-		return (NULL);
+		return (NULL);	
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-	ar = malloc(sizeof(*ar) * i + sizeof(*ar) * j);
+	if (s1 == 0)
+		sz = j + 1;
+	else if (s2 == 0)
+		sz = i + 1;
+	else
+		sz = i + j + 1;
+	ar = malloc(sizeof(*ar) * sz);
 	if (ar == 0)
 		return (NULL);
 	for (sz = 0; sz < i; sz++)
