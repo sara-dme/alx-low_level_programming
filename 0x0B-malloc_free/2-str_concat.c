fab704b5ff6e1bf74adce702a6a8f93433385d4a
sara-dme <s.daoumy@gmail.com>
2023-04-11 05:49:36 -0700

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == 0)
 		s2 = "";
-	else if (s1 == 0)
+	if (s1 == 0)
 		s1 = "";
-	else if (s1 == 0 && s2 == 0)
+	if (s1 == 0 && s2 == 0)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 	{
