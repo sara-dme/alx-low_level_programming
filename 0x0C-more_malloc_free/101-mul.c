a9a05057ef9cda032bdc0a073202f15eec78b928
sara-dme <s.daoumy@gmail.com>
2023-04-14 09:41:25 -0700

    fix the code

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,9 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _isdigit(int a);
+int _isdigit(int *a, int le);
 int _atoi(char *s);
+int *ptr(int n);
+int len(int n);
 
+/** 
+ * ptr - pointer contain num 
+ * @n: number to put into pointer
+ * Return: pointer
+ */
+int *ptr(int n)
+{
+	int l = len(n);
+	int *p = malloc(sizeof(int) * l);
+	int r;
+	int i = 0;
+	if (p == NULL)
+		return (NULL);
+	if (n >= 10)
+	{
+		r = n % 10;
+		p[i] = r;
+		i++;
+		ptr(n / 10);
+	}
+	return (p);
+}
+/**
+ * len -  length
+ * @i: int length
+ * Return: int 
+ */
+int len(int n)
+{
+	int i = 0;
+	if (n >= 10)
+	{
+		len(n / 10);
+		i++;
+	}
+	return (i + 1);
+}
 /**
  * main - check the code for
  * @argc: int argument
@@ -14,17 +53,27 @@ int _atoi(char *s);
 int main(int argc, char *argv[])
 {
 	int num;
-	int a, b, i, j;
+	int *t;
+	int *tt;
+	int a, b, l, ll;
+	unsigned int i, j;
 
 	if (argc != 3)
 	{
-		printf("ERROR!!!!!!!\n");
+		printf("ERROR\n");
 		exit(98);
 	}
-	i = _atoi(argv[0]);
-	j = _atoi(argv[1]);
-	a = _isdigit(i);
-	b = _isdigit(j);
+	i = _atoi(argv[1]);
+	j = _atoi(argv[2]);
+
+	t = ptr(i);
+	tt = ptr(j);
+
+	l = len(i);
+	ll = len(j);
+
+	a = _isdigit(t, l);
+	b = _isdigit(tt, ll);
 	if ((a == 0) || (b == 0))
 	{
 		printf("ERROR\n");
@@ -62,13 +111,17 @@ return (res);
 /**
  * _isdigit - check for digit
  * @a: char to check
+ * @len: length
  * Return: Always 0 or 1
  */
-int _isdigit(int a)
+int _isdigit(int *a,int le)
 {
-	if (a >= '0' && a <= '9')
-		return (1);
-	else 
+	int i;
+
+	for (i = 0; a[i] <= le; i++)
+	{
+	if (a[i] < '0' && a[i] > '9')
 		return (0);
-	return (0);
+	}
+	return (1);
 }
