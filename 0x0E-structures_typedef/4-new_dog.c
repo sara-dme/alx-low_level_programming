7517d40906d63438b0ba6008312ccbfe93bec0bd
sara-dme <s.daoumy@gmail.com>
2023-04-17 06:18:15 -0700

    my new poppy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,6 +7,7 @@
  * @name: name
  * @age: age
  * @owner: owner
+ * Return: pointer to new dog struct or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -17,6 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 	if (d == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 	return (d);
 }
