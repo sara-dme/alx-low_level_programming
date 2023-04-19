c4168c662c6b1709a0e4775b1faed79b17879438
sara-dme <s.daoumy@gmail.com>
2023-04-19 15:54:28 -0700

    op  funct

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function
@@ -11,22 +12,22 @@
  */
 int (*get_op_func(char *s))(int n, int nn)
 {
-	 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
 
-    i = 0;
-    while (i < 5)
-    {
-	    if (ops[i].op == s)
-		    return (ops[i].f);
-	    i++;
-    }
-    return (NULL);
+	i = 0;
+	while (i < 6)
+	{
+	if (strcmp(ops[i].op, s) == 0)
+		return (ops[i].f);
+	i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,20 +10,18 @@
 int main(int argc, char *argv[])
 {
 	int n, nn, res;
-	char s;
-	
-	s = argv[2][0];
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] == NULL)
+	if ((argv[2] == NULL) || (get_op_func(argv[2]) == NULL))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (((s == '/') ||(s == '%')) && (s == '0'))
+	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && (argv[3][0] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
