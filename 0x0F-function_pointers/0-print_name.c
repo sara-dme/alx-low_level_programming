14c4de36c30696e811c71f25e883ccb5c6ba25fd
sara-dme <s.daoumy@gmail.com>
2023-04-19 04:20:31 -0700

    test NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name.
  * @name: name to print
- *
+ * @f: pointer to function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if ((f != NULL) && (name != NULL))
+		f(name);
 }
