e8af80c599953977c679ce0b347540ce74feceed
sara-dme <s.daoumy@gmail.com>
2023-04-27 01:58:31 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list to be printed
+ * Return: size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t sz = 0;
+	
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+		printf("[0] (nil)\n");
+		else
+		printf("[%d] %s\n", h->len, h->str);
+		sz++;
+		h = h->next;
+	}
+	return (sz);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+Learning Objective
+
+
+    When and why using linked lists vs arrays
+    How to build and use linked lists
+
