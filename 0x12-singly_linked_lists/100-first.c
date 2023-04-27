870db3ee49a18e8d50306d758dfc1e0fbfeb1af7
sara-dme <s.daoumy@gmail.com>
2023-04-27 04:29:19 -0700

    betty-doc

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 void before_main(void) __attribute__((constructor));
 
+/**
+ * before_main - print msg before main exec
+ */
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
