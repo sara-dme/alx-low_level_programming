6e0cef598ee4ecceb7baeef1813baab3b1137677
sara-dme <s.daoumy@gmail.com>
2023-04-30 11:28:59 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *lst;
+
+	while (*head)
+	{
+		lst = *head;
+		*head = (*head)->next;
+		free(lst);
+	}
+	*head = NULL;
+}
