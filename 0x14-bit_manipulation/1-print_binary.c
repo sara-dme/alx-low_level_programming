01224ea0685ce74ba2b3ff3669cae0405b6afb8f
sara-dme <s.daoumy@gmail.com>
2023-05-03 23:35:46 -0700

    git bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary
+ * @n: bin to beprinted
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int x;
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+}
