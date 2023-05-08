c568122108515936167b10ce7dac0c7d134fed84
sara-dme <s.daoumy@gmail.com>
2023-05-08 10:33:48 -0700

    fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int ac, char **av)
 {
 	int file_from, file_to, wr, rd, len = 1024;
 	char *buffer;
-
+	
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -36,7 +36,7 @@ int main(int ac, char **av)
 	}
 	if (file_exist(av[1]) != 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (file_exist(av[2]) == 0)
@@ -51,18 +51,18 @@ int main(int ac, char **av)
 	free(buffer);
 	if (wr == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 
 	if (close(file_from) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
 		return (100);
 	}
 	if (close(file_to) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
 		return (100);
 	}
 	return (0);
