28933a7b3da4185b8383b3dfe4f04a9d5feade7f
sara-dme <s.daoumy@gmail.com>
2023-07-10 12:14:59 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,14 +19,20 @@ int main(int ac, char **av)
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from filr%s\n", av[1]), exit(98);
-	while (1)
+	rd = 1024;
+	while (rd == 1024)
 	{
 		rd = read(file_from, buffer, 1024);
 		if (rd == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (rd > 0)
+		{
 		wr = write(file_to, buffer, rd);
 		if (wr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		else
+			break;
+		}
 		if (close(file_from) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from), exit(100);
