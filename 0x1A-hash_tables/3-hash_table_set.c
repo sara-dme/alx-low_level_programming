3f4161f879b438e39c278dcae2c81350b39c2d9d
sara-dme <s.daoumy@gmail.com>
2023-09-14 00:44:23 -0700

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,8 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i, index;
 	char *copy;
 
-       if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	      return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
        copy = strdup(value);
        if (copy == NULL)
