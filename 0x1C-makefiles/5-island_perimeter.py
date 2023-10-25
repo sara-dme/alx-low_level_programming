275f7938b01bddcd36fd5e53e29517fbb8108d32
sara-dme <s.daoumy@gmail.com>
2023-10-25 07:52:51 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,4 +3,9 @@
 
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
-
+    num = 0
+    for row in grid:
+        for col in row:
+            if col == 1:
+                num += 1
+    return ((num * 2) + 2)
